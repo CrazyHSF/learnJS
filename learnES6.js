6419c9df222c00497c12cefbf4e5eda34723276d
CrazyHSF <niushipapa@gmail.com>
2017-04-18 23:05:47 +0800

    Promis 4-18

--- learnES6.js
+++ learnES6.js
@@ -2898,3 +2898,96 @@ console.trace(Object.getOwnPropertySymbols(obj));
 console.trace(Reflect.ownKeys(obj));
 
 
+function timeout(ms){
+    return new  Promise((resolve,reject)=>{
+       setTimeout(resolve,ms,'done');
+    });
+}
+
+timeout(100).then((value)=>{
+    console.log(value);
+});
+
+let promise = new Promise(function (resolve,reject) {
+    console.trace("Promise");
+    resolve();
+});
+
+promise.then(function(){
+   console.trace("Resolved.")
+});
+
+console.trace("hi!");
+
+function loadImageAsync(url){
+    return new Promise(function(resolve,reject){
+        var image = new Image();
+        image.onload = function(){
+            resolve(image);
+        };
+        image.onerror = function(){
+            reject(new Error('could not load image at '+url));
+        };
+        image.src = url;
+    });
+}
+
+loadImageAsync("http://es6.ruanyifeng.com/images/cover-2nd.jpg");
+
+var getJSON = function (url) {
+    var promise = new Promise(function (resolve,reject){
+        var client = new  XMLHttpRequest();
+        client.open('GET',url);
+        client.onreadystatechange = handler;
+        client.responseType = "json";
+        client.setRequestHeader("Accept","application/json");
+        client.send();
+
+        function handler(){
+            if(this.readyState !== 4){
+                return ;
+            }
+            if(this.status === 200){
+                resolve(this.response);
+            } else {
+                reject(new  Error(this.statusText));
+            }
+        };
+    });
+    return promise;
+};
+
+getJSON("http://es6.ruanyifeng.com/#docs/promise").then(function(json){
+    console.log("Contents:" + json);
+},function(error){
+    console.error("error:",error);
+});
+
+var p1 = new  Promise(function(resolve,reject){
+    setTimeout(() => reject(new Error('fail')),3000);
+})
+
+var p2 = new Promise(function(resolve,reject){
+    setTimeout(() => resolve(p1),1000);
+})
+
+p2
+    .then(result => console.trace(result))
+    .catch(error => console.trace(error));
+
+getJSON("/post/1.json").then(function(post){
+    return getJSON(post.commentURL);
+}).then(function funcA(comments){
+    console.trace("Resolved: ",comments);
+},function funcB(err){
+    console.trace("Rejected: ",err);
+});
+
+getJSON("/post/1.json").then(function(post){
+   return getJSon(post.commentsURL);
+}).then(
+    funcA = comments => console.trace("Resolved: ",comments),
+    funcB = comments => console.trace("Rejected: ",err)
+);
+
+
